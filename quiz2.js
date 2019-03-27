0214a091e6757df9b944cf00f7e90477376e1a0a
Denerius Moye <dmoye@techlaunch.io>
2019-03-26 20:42:09 -0400

    first commit

---
+++ quiz2.js
@@ -0,0 +1,21 @@
+var recipeContainer = document.querySelector("#recipeContainer")
+
+recipeContainer.innerHTML += `
+<h3 class ="text-warning"><strong>Making Ice Cream Sandwiches</strong></h3>
+<p class = "lead">Kids and adults alike will be fighting over these big moster ice cream sandwiches! The best part about these things is that they are no bake. You don't even need to warm up your oven if you purchased cookies. Make sure to use the mini M&Ms or the large ones will fall off. As a volunteer field editor for Taste of Home magazine, I love recipes that can be assembled quickly and still make a wonderful presentation. 
+<div class="container-fluid p-3 mb-2 bg-warning text-dark">
+<h4><strong>Ingredients</strong></h4>
+    <ul>
+      <li>Cookies and Cream ice cream</li>
+      <li>Large chocolate chip cookies</li>
+      <li>M&M's Minis</li>
+      <li>Reese's mini peanut butter cups</li>
+    </ul>
+</div>
+<div class= "row">
+    <div class= "col-3"> <img src="ice-cream.jpg" alt="Oreo" width= 250px height= 200px></div>
+    <div class= "col-3"> <img src="cookies.jpg" alt="Cookies" width= 200px height= 200px></div>
+    <div class= "col-3"> <img src="mmminis.jpg" alt="MMM" width= 200px height= 200px></div>
+    <div class= "col-3"> <img src="pb.jpg" alt="Reese's" width= 200px height= 200px></div>
+</div>
+`
